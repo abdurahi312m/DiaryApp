366dc9d97a9a6884db6be8498f036e7e852bf4af
Abdurah1m <talantbekuuluabdurahim3@gmail.com>
2023-10-24 15:57:59 +0600

    client id, application id

--- app/src/main/java/kg/abu/diaryapp/util/Constants.kt
+++ app/src/main/java/kg/abu/diaryapp/util/Constants.kt
@@ -2,6 +2,7 @@ package kg.abu.diaryapp.util
 
 object Constants {
 
-    const val APP_ID = "application-0-szotf"
+    const val APP_ID = "application-0-igfvx"
+    const val CLIENT_ID = "599535370273-fcuc09s99opdslumhnvat9lua0ld7i79.apps.googleusercontent.com"
 
 }
